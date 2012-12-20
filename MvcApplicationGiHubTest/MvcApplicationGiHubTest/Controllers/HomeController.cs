2a761c3f124288f1e606cab41f4f3679b4ab4eda
Ivana Gjorgeva <igjorgeva@gmail.com>
2012-12-20 12:51:52 -0500

    new view bag added.
    some desc

--- MvcApplicationGiHubTest/MvcApplicationGiHubTest/Controllers/HomeController.cs
+++ MvcApplicationGiHubTest/MvcApplicationGiHubTest/Controllers/HomeController.cs
@@ -19,6 +19,7 @@ namespace MvcApplicationGiHubTest.Controllers
         {
             ViewBag.Message = "Your app description page. Plus I have added my own custom text in the view bag's message!";
             ViewBag.Message = "I think I just managed to configure the whole Git/GitHub ****";
+            ViewBag.Message = "Hello there!";
 
             return View();
         }
